148a76a71e72251fe84354846a934c81a4f64601
Rcastro89 <r_castro_m@yahoo.es>
2021-10-01 17:33:14 -0500

    Number separed

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - funtion for putchar
+ * Return: (0) success
+ */
+int main(void)
+{
+	int m;
+	int n;
+	int o;
+
+	for (n = 48; n <= 56; n++)
+	{
+		for (m = 49; m <= 57; m++)
+		{ 
+			for (o = 50; o <=58; o++)
+			{
+				if (m > n || o > m)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(o);
+					if (n != 56 || m != 57 || o != 58)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

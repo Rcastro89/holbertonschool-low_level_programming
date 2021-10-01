00680e944f59fea8569a8c49c7c72e290eef5cae
Rcastro89 <r_castro_m@yahoo.es>
2021-10-01 16:33:11 -0500

    Number separed

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,14 +9,11 @@ int main(void)
 
 	for (n = 48; n < 58; n++)
 	{
+		putchar(n);
 		if (n != 57)
 		{
-			putchar(n);
 			putchar(',');
 			putchar(' ');
-		} else 
-		{
-			putchar(n);
 		}
 	}
 	putchar('\n');

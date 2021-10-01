d043f778aae67d5164de8939ecd9d15c1491249a
Rcastro89 <r_castro_m@yahoo.es>
2021-10-01 16:45:27 -0500

    Number separed

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - funtion for putchar
+ * Return: (0) success
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n < 58; n++)
+	{ 
+		for (m = 48; m < 58; m++)
+		{
+			putchar(n);
+			putchar(m);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}

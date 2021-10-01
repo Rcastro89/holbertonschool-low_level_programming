e0d52f34b779849a00a789c1fef56751e3fc6d5b
Rcastro89 <r_castro_m@yahoo.es>
2021-09-30 22:23:32 -0500

    error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ * main - sizeof
+ * Return: (0) success
+ */
+int main(void)
+{
+	Try
+	{
+		printf("Size of a char: %zd byte(s)\n", sizeof(char));
+		printf("Size of an int: %zd byte(s)\n", sizeof(int));
+		printf("Size of a long int: %zd byte(s)\n", sizeof(long int));
+		printf("Size of a long long int: %zd byte(s)\n", sizeof(long long int));
+		printf("Size of a float: %zd byte(s)\n", sizeof(float));
+		return (0);
+	}
+	Catch (ex)
+	{	
+		printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	}
+}
+if (main() < 0)
+       	Throw ex;

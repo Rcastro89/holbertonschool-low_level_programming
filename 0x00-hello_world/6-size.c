43a43849c8537719bb15c4c279a216513545e9d5
Rcastro89 <r_castro_m@yahoo.es>
2021-09-30 21:37:28 -0500

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,10 @@
 
 int main(void)
 {
-	
-	printf("Entero %zd", sizeof (int));
+	printf("Size of a char: %zd byte(s)", sizeof (char));
+	printf("Size of an int: %zd byte(s)", sizeof (int));
+	printf("Size of a long int: %zd byte(s)", sizeof (long int));
+	printf("Size of a long long int: %zd byte(s)", sizeof (long long int));
+	printf("Size of a float: %zd byte(s)", sizeof (float));
 	return (0);
 }

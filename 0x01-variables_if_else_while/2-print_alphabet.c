9fc66736582e4c3ef487f299e30f5538773b1080
Rcastro89 <r_castro_m@yahoo.es>
2021-10-01 14:40:19 -0500

    abc

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
-
+/**
+ * main - funtion do while putchar
+ * Return: (0) success
+ */
 int main(void)
 {
 	char lt = 'a';
 	char line = '\n';
 	int i = 0;
 
-	do
-	{
+	do {
 		putchar(lt);
 		lt++;
 		i++;
-	} while (i!= 26);
+	} while (i != 26);
 	putchar(line);
+
 	return (0);
 }

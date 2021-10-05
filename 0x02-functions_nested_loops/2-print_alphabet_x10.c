c4b2fbb388f3d4a4269e4b286891626f4f70775f
Rcastro89 <r_castro_m@yahoo.es>
2021-10-04 23:21:34 -0500

    alphabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - alphabeth x10
+ * Return: (0) success
+ */
+void print_alphabet_x10(void)
+{
+    int i;
+    int j;
+	char lt;
+
+	    for (i = 0; i < 10; i++) {
+			lt = 'a';
+			for (j = 0; j < 26; j++){
+				_putchar(lt);
+				lt++;
+			}
+			_putchar('\n');
+		}
+		
+}

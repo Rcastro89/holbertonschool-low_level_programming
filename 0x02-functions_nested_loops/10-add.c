25e6ac97de71fcfca215cb71188339c032177847
Rcastro89 <r_castro_m@yahoo.es>
2021-10-05 19:45:31 -0500

    add number

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - add
+ * @a: integer that cheked
+ * @b: integer that cheked
+ * Return: (k) success
+ */
+int add(int a, int b)
+{
+	int k;
+
+	k = b + a;
+	return (k);
+}

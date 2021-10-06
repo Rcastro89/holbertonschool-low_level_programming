815ec8a0b835cf76668c512507ea4be2dfea78e7
Rcastro89 <r_castro_m@yahoo.es>
2021-10-05 22:41:22 -0500

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * print_to_98 - to 98
@@ -5,44 +6,28 @@
  */
 void print_to_98(int n)
 {
-	int x, y, z, w;
-
-	for (; n <= 98; n++)
+	if (n >= 98)
 	{
-		if (n < 10 && n >= 0)
+		for (; n >= 98; n--)
 		{
-			_putchar(n + '0');
-		} else if (n >= 10 && n < 100)
-		{
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
-		} else if (n < 0 && n > -10)
-		{
-			_putchar('-');
-			x = n * -1;
-			_putchar(x + '0');
-		} else if (n <= -10 && n > -100)
-		{
-			_putchar('-');
-			x = n * -1;
-			_putchar((x / 10) + '0');
-			_putchar((x % 10) + '0');
-		} else if (n <= -100)
-		{
-			w = n * -1;
-			x = w / 100;
-			y = w / 10;
-			z = y % 10;
-			_putchar('-');
-			_putchar(x + '0');
-			_putchar(z + '0');
-			_putchar((w % 10) + '0');
+			if (n != 98)
+			printf("%i, ", n);
+			else
+			{
+				printf("%i", n);
+			}
 		}
-		if (n != 98)
+	} else
+	{
+		for (; n <= 98; n++)
 		{
-			_putchar(',');
-			_putchar(' ');
+			if (n != 98)
+			printf("%i, ", n);
+			else
+			{
+				printf("%i", n);
+			}
 		}
 	}
-	_putchar('\n');
+	printf("\n");
 }

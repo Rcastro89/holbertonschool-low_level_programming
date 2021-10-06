638d786fb99ccd1b5700c9541cb8f2fd9dd8b3de
Rcastro89 <r_castro_m@yahoo.es>
2021-10-06 01:06:29 -0500

    1024

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,32 +10,27 @@ int main(void)
 	int b = 0;
 	int c, n;
 	
-	n = 10;
+	n = 1024;
 	do
 	{
 		b = 3 * a;
 		if (b < n)
 		{
-			printf("%i", b);
-			printf("\n");
 			c = c + b;
 			a++;
 		}
 	} while (b < n);
 	b = 0;
-	printf("\n");
 	for (a = 1; b < n; a++)
 	{
 		b = 5 * a;
 		if (b < n)
 		{
-			printf("%i", b);
-			printf("\n");
 			c = c + b;
 			a++;
 		}
 	}
-	printf("The sum of these multiples is %i ", c);
+	printf("%i", c);
 	printf("\n");
 	return (c);
 }

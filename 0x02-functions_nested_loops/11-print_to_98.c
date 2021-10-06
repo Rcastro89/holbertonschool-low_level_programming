c96b72b7d57a9d22251f4c6e84d41e46074640f3
Rcastro89 <r_castro_m@yahoo.es>
2021-10-05 21:44:59 -0500

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,69 +2,47 @@
 /**
  * print_to_98 - to 98
  * @n: integer
- * Return: Always 0.
  */
 void print_to_98(int n)
 {
-	int x;
-	int y;
-	int z;
+	int x, y, z, w;
 
-	if (n <= 98)
+	for (; n <= 98; n++)
 	{
-		for (; n <= 98; n++)
+		if (n < 10 && n >= 0)
 		{
-			if (n < 10 && n >= 0)
-			{
-				_putchar(n + '0');
-			} else if (n >= 10 && n < 100)
-			{
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
-			} else if (n <  0 && n > -10)
-			{
-				_putchar('-');
-				x = n * -1;
-				_putchar(x + '0');
-			} else if (n <= -10)
-			{
-				_putchar('-');
-				x = n * -1;
-				_putchar((x / 10) + '0');
-				_putchar((x % 10) + '0');
-			}
-			if (n != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar(n + '0');
+		} else if (n >= 10 && n < 100)
+		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+		} else if (n < 0 && n > -10)
+		{
+			_putchar('-');
+			x = n * -1;
+			_putchar(x + '0');
+		} else if (n <= -10 && n > -100)
+		{
+			_putchar('-');
+			x = n * -1;
+			_putchar((x / 10) + '0');
+			_putchar((x % 10) + '0');
+		} else if (n <= -100)
+		{
+			w = n * -1;
+			x = w / 100;
+			y = w / 10;
+			z = y % 10;
+			_putchar('-');
+			_putchar(x + '0');
+			_putchar(z + '0');
+			_putchar((w % 10) + '0');
 		}
-	} else
-	{
-		for (; n >= 98; n--)
+		if (n != 98)
 		{
-			if (n >= 100)
-			{
-				x = n / 100;
-				y = n / 10;
-				z = y % 10;
-				_putchar(x + '0');
-				_putchar(z + '0');
-				_putchar((n % 10) + '0');
-
-			} else if (n < 100)
-			{
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
-			}
-			if (n != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-
+			_putchar(',');
+			_putchar(' ');
 		}
 	}
 	_putchar('\n');
-
 }

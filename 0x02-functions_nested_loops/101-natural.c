96b0dcab348a418cc25825a3790cf8b848d6dd0e
Rcastro89 <r_castro_m@yahoo.es>
2021-10-06 00:47:52 -0500

    1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_last_digit - return last number of digit
+ * @n: integer that cheked
+ * Return: (1) success
+ */
+int print_natural(int n)
+{
+	int a;
+	int b, c;
+	
+	for (a = 1; b < n; a++)
+	{
+		b = 3 * a;
+		printf("%i", b);
+		printf("\n");
+		c = c + b;
+	}
+	b = 0;
+	printf("\n");
+	for (a = 1; b < n; a++)
+	{
+		b = 5 * a;
+		printf("%i", b);
+		printf("\n");
+		c = c + b;
+	}
+	printf("The sum of these multiples is %i ", c);
+	printf("\n");
+	return (c);
+}

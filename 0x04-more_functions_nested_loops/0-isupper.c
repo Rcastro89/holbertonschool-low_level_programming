881eb53b9bf5ed0a99e2fb10a471f9c315f4b194
Rcastro89 <r_castro_m@yahoo.es>
2021-10-07 23:06:53 -0500

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isupper - return true if uppcase leter
+ * @a: char to evaluate
+ * Return: (0) success
+ */
+
+int _isupper(char a)
+{
+
+	if (isupper(a))
+	{
+		return (1);
+	}
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,5 +4,6 @@
 #include <stdio.h>
 
 int _putchar(char c);
+int _isupper(char a);
 
 #endif /* MAIN_H */

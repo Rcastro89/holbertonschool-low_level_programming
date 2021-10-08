656479eacc106bd184d70c35cbfce10f1f40b06c
Rcastro89 <r_castro_m@yahoo.es>
2021-10-08 11:31:01 -0500

    Print Numbers except two

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * more_numbers - print number of 0 to 14 - 10 times
+ */
+void more_numbers(void)
+{
+	int i = 0;
+	int j;
+
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 15)
+		{
+			if (j < 10)
+			{
+				_putchar(j + '0');
+			} else
+			{
+				_putchar(j / 10 + '0');
+				_putchar(j % 10 + '0');
+			}
+		j++;
+		}
+		i++;
+		printf("\n");
+	}
+}

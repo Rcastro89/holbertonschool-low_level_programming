24e7f1dac94c4825167792954d8b8262d6731e4e
Rcastro89 <r_castro_m@yahoo.es>
2021-10-08 12:20:15 -0500

    lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line - print line
+ * @n: numbers of lines to print
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n != 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}

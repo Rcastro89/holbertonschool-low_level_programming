c268ba2ac859e2c123b5653d08b62d79b3f2b1bc
Rcastro89 <r_castro_m@yahoo.es>
2021-10-11 11:38:30 -0500

    count string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include<string.h>
+/**
+ * _strlen - count letters
+ * @s: string to evaluate
+ * Return: (count) success
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	count = strlen(s);
+	return (count);
+}

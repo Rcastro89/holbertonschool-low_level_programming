0bbbf67cd11a85b4b1b7596f353ae9b217413280
Rcastro89 <r_castro_m@yahoo.es>
2021-10-11 22:15:40 -0500

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include<string.h>
+#include<stdio.h>
+/**
+ * puts_half - write only half
+ * @str: string to evaluate
+ */
+void puts_half(char *str)
+{
+	int count, i, j;
+	char temp[10000];
+
+	count = strlen(str);
+	j = 0;
+
+	if (count % 2 == 0)
+	{
+		for (i = count; i > (count / 2); i--)
+		{
+			temp[j] = str[((count / 2) + j)];
+			j++;
+		}
+	} else
+	{
+		for (i = count; i > ((count - 1) / 2); i--)
+		{
+			temp[j] = str[((count / 2) + j)];
+			j++;
+		}
+	}
+	puts(temp);
+}

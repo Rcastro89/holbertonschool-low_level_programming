5d86f2d31b490dd145301afc52d4cc80359955f2
Rcastro89 <r_castro_m@yahoo.es>
2021-10-14 16:30:40 -0500

    capital

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -21,6 +21,8 @@ char *cap_string(char *a)
 		temp == 41 || temp == 44 || temp == 46 || temp == 59 ||
 		temp == 63 || temp == 123 || temp == 125)
 		{
+			if (a[0] > 96 && a[0] < 123)
+				a[0] = a[0] - 32;
 			temp = a[j + 1];
 			if (temp > 96 && temp < 122)
 			{

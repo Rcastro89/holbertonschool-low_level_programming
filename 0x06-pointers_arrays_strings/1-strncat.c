9d98acaaa58c4657cf3894497f0c828ca5799ab4
Rcastro89 <r_castro_m@yahoo.es>
2021-10-14 12:07:41 -0500

    concatec strings bytes

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,8 +13,12 @@ char *_strncat(char *dest, char *src, int n)
 	i = 0, j = 0;
 	while (dest[i] != '\0')
 		i++;
-	while (j != n)
+	while (src[j] != '\0')
 	{
+		if (j == n)
+		{
+			break;
+		}
 		dest[i] = src[j];
 		j++;
 		i++;

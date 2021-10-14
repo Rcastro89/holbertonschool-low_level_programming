98b85b2c49b1b539a79b020acd82b5a5ff3b2bb7
Rcastro89 <r_castro_m@yahoo.es>
2021-10-14 12:49:19 -0500

    compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,11 +7,12 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 == *s2 && *s1 != '\0')
-	{
-		s1++;
-		s2++;
-	}
-	return (s1 - s2);
-}
+	int i;
 
+	i = 0;
+	while (s1[i] == s2[i] && s1[i] != '\0')
+	{
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}

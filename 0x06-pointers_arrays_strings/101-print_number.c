c1b6ca17fc920f6a2594a08091fd34124223c37e
Rcastro89 <r_castro_m@yahoo.es>
2021-10-14 23:58:17 -0500

    integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,45 @@
+#include"main.h"
+#include<stdio.h>
+/**
+* print_number - print integer
+* @n: integer check
+*/
+void print_number(int n)
+{
+	int i, e, d, div, t, x, y;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		n = n * -1;
+	}
+	else
+	{
+		i = n;
+		t = 0;
+		while (i != 0)
+		{
+			i = i / 10;
+			t++;
+		}
+		if (n < 0)
+		{
+			_putchar('-');
+			n = n * -1;
+		}
+		x = t - 1;
+		for (div = 1; x != 0; x--)
+		div = div * 10;
+		y = n;
+		d = n;
+		while (t != 0)
+		{
+			e = y / div;
+			d = d % div;
+			y = d;
+			t--;
+			div = div / 10;
+			_putchar(e + '0');
+		}
+	}
+}

2d77599ccdada0a4a7ad5e4dcba5271f53648fa4
Rcastro89 <r_castro_m@yahoo.es>
2021-10-18 22:56:27 -0500

    comparate

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,35 @@
+#include"main.h"
+/**
+* _strchr - compare chars
+* @s: string evaluate
+* @c: caracter find
+* Return: (result) success
+*/
+char *_strchr(char *s, char c)
+{
+	int leng = 0;
+	int i, j;
+	int count = 0;
+	char *result;
+	char temp[100];
+
+	while (s[leng] != '\0')
+	{
+		leng++;
+	}
+	for (i = 0; i <= leng; i++)
+	{
+		if (s[i] == c)
+		{
+			count = leng - i;
+			for (j = 0; j <= count; j++)
+			{
+				temp[j] = s[i];
+				i++;
+			}
+			result = temp;
+			return (result);
+		}
+	}
+	return (0);
+}

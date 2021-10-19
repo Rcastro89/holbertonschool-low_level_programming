201aa2c358d4c89ece979a772deee7414f72b66a
Rcastro89 <r_castro_m@yahoo.es>
2021-10-18 22:09:40 -0500

    accept

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,15 +9,15 @@ unsigned int _strspn(char *s, char *accept)
 {
 	int i;
 	int j;
-	int count = 0;
+	unsigned int count = 0;
 
 	i = 0;
-	while (accept[i] != '\0')
+	while (s[i] != '\0')
 	{
 		j = 0;
-		while (s[j] != '\0')
+		while (accept[j] != '\0')
 		{
-			if (accept[i] == s[j])
+			if (s[i] == accept[j])
 			{
 				count++;
 				break;

e345bfbb3b3bba21bb4d86c8121c5d73b5de7199
Rcastro89 <r_castro_m@yahoo.es>
2021-10-19 00:44:35 -0500

    rev

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+* _strstr - compare chars
+* @haystack: string evaluate
+* @needle: caracter find
+* Return: (result) success
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	int leng = 0;
+	int i, j;
+
+	while (needle[leng] != '\0')
+	{
+		leng++;
+	}
+	for (i = 0; i <= leng; i++)
+	{
+		for (j = 0; haystack[j] != '\0'; j++)
+		{
+			if (needle[i] == haystack[j])
+				return (haystack + j);
+		}
+	}
+	return ('\0');
+}

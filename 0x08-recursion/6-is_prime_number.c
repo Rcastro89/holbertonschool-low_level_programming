3cff4c59665acb53e31eec0c94269389b8ebf7d5
Rcastro89 <r_castro_m@yahoo.es>
2021-10-21 14:23:42 -0500

    square

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,26 +1,28 @@
 #include"main.h"
 /**
-* is_prime_number - return 1 if the number is prime
 * primos - calculate prime
-* @n: integer to evaluate
 * @x: value to x
 * @div: divisor
 * Return: (1 or 0) case
 */
 int primos(int x, int div)
 {
-    if (x % div == 0 && x > div)
-    return(1 + primos(x, div + 1));
-    if (x % div == 0 && x == div)
-    return(1);
-    return(primos(x, div + 1));
+	if (x % div == 0 && x > div)
+	return (1 + primos(x, div + 1));
+	if (x % div == 0 && x == div)
+	return (1);
+	return (primos(x, div + 1));
 }
-
+/**
+* is_prime_number - calculate prime
+* @n: integer
+* Return: (1 or 0) case
+*/
 int is_prime_number(int n)
 {
-    if (n < 0)
-    return(0);
-    if (primos(n, 1) == 2)
-    return(1);
-    return(0);
+	if (n < 0)
+	return (0);
+	if (primos(n, 1) == 2)
+	return (1);
+	return (0);
 }

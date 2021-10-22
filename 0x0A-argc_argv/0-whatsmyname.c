df96ebea648009797e89f9a710b06ca40dc448bf
Rcastro89 <r_castro_m@yahoo.es>
2021-10-22 12:04:02 -0500

    Argc Argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+* main - myname
+* @argc: numers of arguments
+* @argv: matriz unideminsional
+* Return: (0) success
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}

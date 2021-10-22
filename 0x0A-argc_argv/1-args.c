f7d3afb02f2e71f528d600c14bce2bea3772788f
Rcastro89 <r_castro_m@yahoo.es>
2021-10-22 12:32:39 -0500

    count argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+* main - myname
+* @argc: numers of arguments
+* @argv: matriz unideminsional
+* Return: (0) success
+*/
+int main(int argc, char __attribute__ ((unused))*argv[])
+{
+	printf("%d\n", (argc - 1));
+	return (0);
+}

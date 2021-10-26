9d3372ff00f05f23c903acbd55f2732f59e9e1ad
Rcastro89 <r_castro_m@yahoo.es>
2021-10-26 03:26:46 -0500

    concatenate whit malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,9 +15,7 @@ char *str_concat(char *s1, char *s2)
 	if (ma == 0)
 	return (NULL);
 	if (s1 == NULL && s2 == NULL)
-	{
-		return (ma);
-	}
+	return (ma);
 	if (s1 == NULL)
 	{
 		while (s2[s2j] != '\0')
@@ -36,6 +34,11 @@ char *str_concat(char *s1, char *s2)
 	}
 	else
 	{
+		while (s2[s2j] != '\0')
+		s2j++;
+		while (s1[s1i] != '\0')
+		s1i++;
+		ma = malloc(s1i + s2j + 1);
 		for (i = 0; i < s1i; i++)
 		ma[i] = s1[i];
 		for (j = 0; j < s2j; j++)

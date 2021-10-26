3fd7ace8192a1c3df45cef48e8a44aa750297567
Rcastro89 <r_castro_m@yahoo.es>
2021-10-26 11:19:10 -0500

    grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,7 +12,8 @@ int **alloc_grid(int width, int height)
 	int **ma;
 
 	ma = (int **) malloc(sizeof(int *) * width);
-
+	if (ma == NULL);
+	return (NULL);
 	for (i = 0; i < width; i++)
 	{
 		ma[i] = malloc(sizeof(int) * height);

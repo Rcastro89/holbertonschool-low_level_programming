585b3dac58f3c88f2fac39f486dcc8c03ad5f9a3
Rcastro89 <r_castro_m@yahoo.es>
2021-10-26 01:57:48 -0500

    first malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,13 +12,9 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	ma = malloc(sizeof(char) * size);
-	if (size == 0)
-	return (NULL);
-	if (*ma == 0)
+	if (size == 0 || *ma == 0)
 	return (NULL);
 	for (i = 0; i < size; i++)
-	{
-		ma[i] = c;
-	}
+	ma[i] = c;
 	return (ma);
 }

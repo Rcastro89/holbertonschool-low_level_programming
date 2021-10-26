03ff427ca5212461c930379bbdefc3a2a5b8b873
Rcastro89 <r_castro_m@yahoo.es>
2021-10-26 11:32:55 -0500

    grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,5 +1,6 @@
-#include"main.h"
-#include<stdlib.h>
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * alloc_grid - grid of integers
  * @width: int width
@@ -8,26 +9,30 @@
 */
 int **alloc_grid(int width, int height)
 {
-	int i, j;
+	short int i, j;
 	int **ma;
 
-	if (height <= 0 || width <= 0)
-	return (NULL);
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
 	ma = (int **) malloc(sizeof(int *) * height);
 	if (ma == NULL)
-	return (NULL);
+		return (NULL);
 	for (i = 0; i < height; i++)
 	{
 		ma[i] = (int *) malloc(sizeof(int) * width);
 		if (ma[i] == NULL)
 		{
+			for (; i >= 0; i--)
+				free(ma[i]);
 			free(ma);
 			return (NULL);
 		}
 	}
-	for (i = 0; i <= height; i++)
+
+	for (i = 0; i < height; i++)
 	{
-		for (j = 0; j <= width; j++)
+		for (j = 0; j < width; j++)
 		{
 			ma[i][j] = 0;
 		}

d9f8f90035c7483c1141f3f41ce3945d03d7ac91
Rcastro89 <r_castro_m@yahoo.es>
2021-10-26 02:31:23 -0500

    duplicate whit malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,10 +10,12 @@ char *_strdup(char *str)
 	char *ma;
 	int i = 0, j;
 
+	if (str == NULL)
+	return (NULL);
 	while (str[i] != '\0')
 	i++;
 	ma = malloc(sizeof(char) * i + 1);
-	if (ma == 0 || !str)
+	if (ma == 0)
 	return (NULL);
 	for (j = 0; j < i; j++)
 	ma[j] = str[j];

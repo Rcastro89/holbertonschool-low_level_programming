8b1f755c1065f40dc4a04e2ae70e07887014a340
Rcastro89 <r_castro_m@yahoo.es>
2021-10-26 03:16:36 -0500

    concatenate whit malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,21 +11,38 @@ char *str_concat(char *s1, char *s2)
 	char *ma;
 	int s1i = 0, s2j = 0, i, j;
 
-	if (s1 == NULL && s2 == NULL)
-	return (NULL);
-	while (s1[s1i] != '\0')
-	s1i++;
-	while (s2[s2j] != '\0')
-	s2j++;
 	ma = malloc(s1i + s2j + 1);
 	if (ma == 0)
 	return (NULL);
-	for (i = 0; i < s1i; i++)
-	ma[i] = s1[i];
-	for (j = 0; j < s2j; j++)
+	if (s1 == NULL && s2 == NULL)
 	{
-		ma[i] = s2[j];
-		i++;
+		return (ma);
+	}
+	if (s1 == NULL)
+	{
+		while (s2[s2j] != '\0')
+		s2j++;
+		ma = malloc(s2j + 1);
+		for (i = 0; i < s2j; i++)
+		ma[i] = s2[i];
+	}
+	else if (s2 == NULL)
+	{
+		while (s1[s1i] != '\0')
+		s1i++;
+		ma = malloc(s1i + 1);
+		for (i = 0; i < s1i; i++)
+		ma[i] = s1[i];
+	}
+	else
+	{
+		for (i = 0; i < s1i; i++)
+		ma[i] = s1[i];
+		for (j = 0; j < s2j; j++)
+		{
+			ma[i] = s2[j];
+			i++;
+		}
 	}
 	return (ma);
 }

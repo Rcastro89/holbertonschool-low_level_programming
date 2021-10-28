dd5964f577b795e0e3c1efeb8ac4c880ec62d197
Rcastro89 <r_castro_m@yahoo.es>
2021-10-27 22:02:28 -0500

    fibonacci 3

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * main - check the code.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long int i = 2, a = 1, b = 2;
+	long int c = 0;
+
+	printf("%ld, %ld, ", a, b);
+	for (; i < 98; i++)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		if (i < 97)
+			printf("%ld, ", c);
+		if (i == 97)
+			printf("%ld\n", c);
+	}
+	return (0);
+}

f6ed402f37e893ff7dcf50a9f273a2bf1994665e
Rcastro89 <r_castro_m@yahoo.es>
2021-10-27 20:43:47 -0500

    add condional

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,30 +1,31 @@
 #include <stdio.h>
+#include "main.h"
 /**
- * print_last_digit - return last number of digit
- * @n: integer that cheked
- * Return: (1) success
+ * print_natural - add number naturals
+ * Return: (d) success
  */
-int main(void)
+int print_natural(void)
 {
-	int a = 1;
-	int b = 0;
-	int c, d, e;
-	int n = 1024;
-	
-	do
-	{
+	unsigned int a = 1;
+	unsigned int b = 0;
+	unsigned int c = 0, d = 0, e = 0;
+	unsigned int n = 1024;
+
+	do {
 		b = 3 * a;
 		if (b < n)
 		{
-			c = c + b;
+			if (b % 5 != 0)
+			{
+				c = c + b;
+			}
 			a++;
 		}
 	} while (b < n);
 	b = 0;
 	d = c;
 	a = 1;
-	do
-	{
+	do {
 		b = 5 * a;
 		if (b < n)
 		{
@@ -33,8 +34,5 @@ int main(void)
 		}
 	} while (b < n);
 	d = d + e;
-	/*d = d - 35193;*/
-	printf("%i", d);
-	printf("\n");
-	return (0);
+	return (d);
 }

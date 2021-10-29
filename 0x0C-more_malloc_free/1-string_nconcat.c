2ae4b444ebc3321a31a827b39bf9e7643d109d40
Rcastro89 <r_castro_m@yahoo.es>
2021-10-28 22:40:34 -0500

    concat two string whit n chars

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,40 +1,6 @@
 #include"main.h"
 #include<stdlib.h>
 /**
- * cuentacadena2 - count the length of the string
- * @cad: string
- * @n: length comparate
- * Return: (cpy) success
-*/
-int cuentacadena2(char *cad, unsigned int n)
-{
-	unsigned int len = 0, cpy = 0;
-
-	while (cad[len] != '\0')
-		len++;
-	if (n >= len)
-	{
-		cpy = len;
-	} else
-	{
-		cpy = n;
-	}
-	return (cpy);
-}
-/**
- * cuentacadena1 - count the length of the string
- * @cad: string
- * Return: (len) success
-*/
-unsigned int cuentacadena1(char *cad)
-{
-	unsigned int len = 0;
-
-	while (cad[len] != '\0')
-		len++;
-	return (len);
-}
-/**
  * string_nconcat - concatenate string whit reserve malloc
  * @s1: first string
  * @s2: second string
@@ -44,24 +10,30 @@ unsigned int cuentacadena1(char *cad)
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ma;
-	unsigned int s1i = 0, s2j = 0, i, j;
+	unsigned int i = 0, j = 0, cpy, x = 0, y = 0;
 
-	ma = malloc(s1i + s2j + 1);
-	if (ma == 0)
-	return (NULL);
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	s2j = cuentacadena2(s2, n);
-	s1i = cuentacadena1(s1);
-	ma = malloc(sizeof(char) * (s1i + s2j) + 1);
+	while (s2[i] != '\0')
+		i++;
+	if (n >= i)
+	{
+		cpy = i;
+	} else
+	{
+		cpy = n;
+	}
+	while (s1[j] != '\0')
+		j++;
+	ma = malloc(sizeof(char) * (j + cpy) + 1);
 	if (ma == NULL)
 		return (NULL);
-	for (i = 0; i < s1i; i++)
-		ma[i] = s1[i];
-	for (j = 0; j < s2j; j++, i++)
-		ma[i] = s2[j];
-	ma[i] = '\0';
+	for (; s1[x] != '\0'; x++)
+		ma[x] = s1[x];
+	for (; y < cpy; y++, x++)
+		ma[x] = s2[y];
+	ma[x] = '\0';
 	return (ma);
 }

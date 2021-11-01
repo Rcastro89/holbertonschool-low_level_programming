5560ff3964bb2783faa8821fd0143d02e050982e
Rcastro89 <r_castro_m@yahoo.es>
2021-11-01 15:52:48 -0500

    print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,14 +11,15 @@ void print_dog(struct dog *d)
 	float can_age = d->age;
 	char *can_owner = d->owner;
 
-	if (d->name == NULL)
-		printf("Name: (nil)\nAge: %f\nOwner: %s\n", can_age, can_owner);
-	else if (d->owner == NULL)
-		printf("Name: %s\nAge: %f\nOwner: (nil)\n", can_name, can_age);
-	else if (d->age == 0)
-		printf("Name: %s\nAge: (nil)\nOwner: %s\n", can_name, can_owner);
-	else if (d->owner == NULL && d->name == NULL)
-		printf("Name: (nil)\nAge: %f\nOwner: (nil)\n", can_age);
-	else
-		printf("Name: %s\nAge: %f\nOwner: %s\n", can_name, can_age, can_owner);
+	if (d != 0)
+	{
+		if (d->name == NULL)
+			printf("Name: (nil)\nAge: %f\nOwner: %s\n", can_age, can_owner);
+		else if (d->owner == NULL)
+			printf("Name: %s\nAge: %f\nOwner: (nil)\n", can_name, can_age);
+		else if (d->age < 0)
+			printf("Name: %s\nAge: (nil)\nOwner: %s\n", can_name, can_owner);
+		else
+			printf("Name: %s\nAge: %f\nOwner: %s\n", can_name, can_age, can_owner);
+	}
 }

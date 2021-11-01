a82d1ea84482996e6ec28d2ce704f10adad51cac
Rcastro89 <r_castro_m@yahoo.es>
2021-11-01 14:58:05 -0500

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - data of can
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} can;
+
+
+can *my_dog(char *name, float age, char *owner);
+
+#endif

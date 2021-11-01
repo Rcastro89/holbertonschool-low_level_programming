3c2f499ae4bdf6075213c4c129ad765b0ec59446
Rcastro89 <r_castro_m@yahoo.es>
2021-11-01 15:49:08 -0500

    print dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include"dog.h"
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * print_dog - print data of dog
+ * @d: struct
+ */
+void print_dog(struct dog *d)
+{
+	char *can_name = d->name;
+	float can_age = d->age;
+	char *can_owner = d->owner;
+
+	if (d->name == NULL)
+		printf("Name: (nil)\nAge: %f\nOwner: %s\n", can_age, can_owner);
+	else if (d->owner == NULL)
+		printf("Name: %s\nAge: %f\nOwner: (nil)\n", can_name, can_age);
+	else if (d->age == 0)
+		printf("Name: %s\nAge: (nil)\nOwner: %s\n", can_name, can_owner);
+	else
+		printf("Name: %s\nAge: %f\nOwner: %s\n", can_name, can_age, can_owner);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,5 +16,6 @@ typedef struct dog
 
 can *my_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif

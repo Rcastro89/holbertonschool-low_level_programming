1455e83e9803f0764e6a2969a0b24c5dd79090c6
Rcastro89 <r_castro_m@yahoo.es>
2021-11-01 17:46:08 -0500

    add can

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,42 @@
+#include"dog.h"
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * new_dog - add new can
+ * @name: name dog
+ * @age: dog's age
+ * @owner: dog amos
+ * Return: (can) success
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *can;
+	int lenname, i, lenowner;
+	float age1;
+	char *name1, *owner1;
+
+	can = malloc(sizeof(dog_t));
+	if (can == NULL)
+		return (NULL);
+	for (lenname = 0; name[lenname] != '\0'; lenname++)
+		;
+	for (lenowner = 0; name[lenowner] != '\0'; lenowner++)
+		;
+	name1 = malloc(sizeof(char) * (lenname + 1));
+	if (name1 == NULL)
+		return (NULL);
+	owner1 = malloc(sizeof(char) * (lenowner + 1));
+	if (owner1 == NULL)
+		return (NULL);
+	for (i = 0; i < lenname; i++)
+		name1[i] = name[i];
+	name1[i] = '\0';
+	for (i = 0; i < lenowner; i++)
+		owner1[i] = owner[i];
+	owner1[i] = '\0';
+	age1 = age;
+	can->age = age1;
+	can->name = name1;
+	can->owner = owner1;
+	return (can);
+}

957b1dc4dff7b2e7ce8b5dfdce1c82f267d0f971
Rcastro89 <r_castro_m@yahoo.es>
2021-11-03 18:57:54 -0500

    operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include "3-calc.h"
+/**
+ * main - calculate
+ * @argc: number of arguments
+ * @argv: array arguments
+ * Return: (0) success
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+	int b;
+	char *op;
+	int result;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	op = argv[2];
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	if (*op != '+' && *op != '-' && *op != '*' && *op != '/' && *op != '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*op == '/' || *op == '%') && (b == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	result = get_op_func(op)(a, b);
+	printf("%d\n", result);
+	printf("\n(((%d)))\n", argc);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - add numbers
+ * @a: number one
+ * @b: number two
+ * Return: operation result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - add numbers
+ * @a: number one
+ * @b: number two
+ * Return: operation result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - add numbers
+ * @a: number one
+ * @b: number two
+ * Return: operation result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - add numbers
+ * @a: number one
+ * @b: number two
+ * Return: operation result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - add numbers
+ * @a: number one
+ * @b: number two
+ * Return: operation result
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

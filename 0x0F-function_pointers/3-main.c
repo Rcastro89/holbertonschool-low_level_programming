eaea1ffebec71ce9584cb92ae97321abbe995d99
Rcastro89 <r_castro_m@yahoo.es>
2021-11-03 20:06:07 -0500

    error control

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,29 +9,26 @@ int main(int argc, char *argv[])
 {
 	int a;
 	int b;
-	char op;
-	int result;
-
+	
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	op = *argv[2];
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	if (op != '+' && op != '-' && op != '*' &&
-		op != '/' && op != '%')
+	
+	if (*argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*' &&
+		*argv[2] != '/' && *argv[2] != '%')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((op == '/' || op == '%') && (b == 0))
+	if ((*argv[2] == '/' || *argv[2] == '%') && (*argv[3] == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	result = get_op_func(argv[2])(a, b);
-	printf("%d\n", result);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	printf("%d\n", get_op_func(argv[2])(a, b));
 	return (0);
 }

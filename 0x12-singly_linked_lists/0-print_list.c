f193dbe619cba11a862110f88e52c4bfd30086b7
Rcastro89 <r_castro_m@yahoo.es>
2021-11-11 14:47:17 -0500

    corrections betty

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,23 +1,24 @@
 #include "lists.h"
 /**
- * 
- * 
+ * print_list - print list struct
+ * @h: struct type list_t
+ * Return: (count nodes)
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t n = 0; 
-	
+	size_t n = 0;
+
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 		{
 			printf("[0] (nil)\n");
-		} 
+		}
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		n++;
 		h = h->next;
 	}
-	return(n);
+	return (n);
 }

66a7ed06cca76c4120479b9d2272e9b53c1422fd
Rcastro89 <r_castro_m@yahoo.es>
2021-11-11 17:14:27 -0500

    corrections betty

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * add_node - add dates of new node
+ * add_node_end - add dates of new node
  * @head: struct type list_t
  * @str: string to add
  * Return: (list_t)
@@ -15,7 +15,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (endnode == NULL)
 		return (NULL);
 	for (len = 0; str[len] != '\0'; len++)
-			;
+		;
 	if (*head == NULL)
 	{
 		endnode->str = strdup(str);

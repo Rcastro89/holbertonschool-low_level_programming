4c2615b1c376174291de29334d824f0463a6ed7e
Rcastro89 <r_castro_m@yahoo.es>
2021-11-16 00:26:18 -0500

    add 9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - insert nwe node in idx position
+ * @head: list linked
+ * @n: date to add
+ * @idx: position to insert
+ * Return: (new node)
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int j = 0;
+	listint_t *new_list;
+	listint_t *list_inter = *head;
+
+	new_list = malloc(sizeof(listint_t));
+	if (new_list == NULL)
+		return (NULL);
+	new_list->n = n;
+	while (list_inter != NULL && j < idx)
+	{
+		if (j == (idx - 1))
+		{
+			new_list->next = list_inter->next;
+			list_inter->next = new_list;
+			return (new_list);
+		}
+		else
+			list_inter = list_inter->next;
+		j++;
+	}
+	return (NULL);
+}

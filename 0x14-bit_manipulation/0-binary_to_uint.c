1eaa2098c9120d78de8037da2385a07c7cc139a5
Rcastro89 <r_castro_m@yahoo.es>
2021-11-18 13:15:58 -0500

    conver binary to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * binary_to_uint - convert binari to decimal
+ * @b: binari to evaluate
+ * Return: decimal
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int one = 2, sum = 0;
+	int i, j, lenght = 0, multi = 0;
+
+	if (b == NULL)
+		return (1);
+	lenght = strlen(b) - 1;
+	multi = lenght;
+	for (i = 0; i <= lenght; i++, multi--)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		if (b[i] == '1')
+		{
+			for (j = 1; j < multi; j++)
+				one = one * 2;
+			if (i != lenght)
+				sum += one;
+			else
+				sum += 1;
+			one = 2;
+		}
+	}
+	return (sum);
+}

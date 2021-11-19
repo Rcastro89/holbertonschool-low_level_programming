29a8da4c8ec65dd4fac4645fb37d048b953760bd
Rcastro89 <r_castro_m@yahoo.es>
2021-11-19 15:05:14 -0500

    endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * get_endianness - Get the endianness object
+ * @return int 
+ */
+int get_endianness(void)
+{
+	int16_t i = 1;
+   	int8_t *p = (int8_t *) &i;
+
+	return (p[0]);
+}

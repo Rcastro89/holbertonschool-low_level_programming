5275a94d36be4194f0acb85ed8bf6fecf2c9b2c8
Rcastro89 <r_castro_m@yahoo.es>
2021-11-23 00:53:28 -0500

    control error

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -27,7 +27,15 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
+
 	ctrread = read(op_from, buf, 1024);
+	for (; ctrread > 0; ctrread = read(op_from, buf, 1024))
+	{
+		ctrread = write(op_to, buf, ctrread);
+		if (ctrread < 0)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	}
+
 	if (ctrread == -1)
 		return (-1);
 	ctrwrite = write(op_to, buf, ctrread);
@@ -47,3 +55,5 @@ int main(int argc, char *argv[])
 	}
 	return (0);
 }
+
+

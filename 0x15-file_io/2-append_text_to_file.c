4a4e3ce44fa799859cfe053a00c823f4e4c9b212
Rcastro89 <r_castro_m@yahoo.es>
2021-11-23 00:20:51 -0500

    control error

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,7 +16,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		for (i = 0; text_content[i] != '\0'; i++)
 			;
-		write(add, text_content, (i + 1));
+		write(add, text_content, i);
 	}
 	close(add);
 	return (1);

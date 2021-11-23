9960ab4d54eaef9022771cd9f4ff66e31fd217ed
Rcastro89 <r_castro_m@yahoo.es>
2021-11-22 19:02:24 -0500

    copy files

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * main - function to copy files
+ * @argc: numbers of parametres
+ * @argv: files from and to
+ * Return: (0) success 
+ */
+int main(int argc, char *argv[])
+{
+	int op_from, op_to, ctrread, ctrwrite, ctrclose;
+	char buf[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	op_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (op_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	op_from = open(argv[1], O_RDONLY);
+	if (op_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	ctrread = read(op_from, buf, 1024);
+	if (ctrread == -1)
+		return (-1);	
+	ctrwrite = write(op_to, buf, ctrread);
+	if (ctrwrite == -1)
+		return (-1);
+	ctrclose = close(op_to);
+	if (op_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ctrclose);
+		exit(100);
+	}
+	ctrclose = close(op_from);
+	if (op_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ctrclose);
+		exit(100);
+	}
+	return (0);
+}

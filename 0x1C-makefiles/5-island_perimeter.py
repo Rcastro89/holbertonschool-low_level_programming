e7fd82494a232394481fdd2426753566f985d56a
Rcastro89 <r_castro_m@yahoo.es>
2022-03-21 16:29:30 -0500

    5 isla perimert

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,16 +2,27 @@
 """validar perimetros"""
 
 
+def validar_rectangulo(grid):
+    """validar rectangulo"""
+    alto = len(grid)
+    largoreal = len(grid[0])
+    for fila in range(len(grid)):
+        largo = len(grid[fila])
+        if largo != largoreal:
+            return -1
+        if largo == alto:
+            return -1
+    return 0
+
+
 def island_perimeter(grid):
     """sumar perimetros"""
     var = 0
     if len(grid) < 100 and len(grid) > 0:
         var = 0
-        largoreal = len(grid[0])
+        if validar_rectangulo(grid) == -1:
+            return 0
         for fila in range(len(grid)):
-            largo = len(grid[fila])
-            if largo != largoreal:
-                return 0
             if len(grid[fila]) > 100 or len(grid[fila]) == 0:
                 return 0
             for colum in range(len(grid[fila])):

67e9608014fda5fd2081e138db38466dd367452e
Rcastro89 <r_castro_m@yahoo.es>
2022-03-21 17:22:38 -0500

    5 isla perimert

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,18 +4,16 @@
 
 def island_perimeter(grid):
     """sumar perimetros"""
-    rows = len(grid)
     var = 0
-    for i in range(rows):
-        cols = len(grid[i])
-        for j in range(cols):
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
             if (grid[i][j] == 1):
                 if j == 0 or grid[i][j - 1] == 0:
                     var = var + 1
                 if i == 0 or grid[i - 1][j] == 0:
                     var = var + 1
-                if j == cols - 1 or grid[i][j + 1] == 0:
+                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
                     var = var + 1
-                if i == rows - 1 or grid[i + 1][j] == 0:
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
                     var = var + 1
     return var

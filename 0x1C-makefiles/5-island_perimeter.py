a74d4ed3e9b8266fc832ba13144c001510480944
Rcastro89 <r_castro_m@yahoo.es>
2022-03-21 15:55:04 -0500

    5 isla perimert

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
 """validar perimetros"""
+
+
 def island_perimeter(grid):
     """sumar perimetros"""
-    var = 0
-    for fila in range(len(grid)):
-        for colum in range(len(grid[fila])):
-            if grid[fila][colum] == 1:
-                try:
-                    if grid[fila-1][colum] == 0:
-                        var = var + 1
-                except Exception:
-                    pass
-                try:
-                    if grid[fila][colum-1] == 0:
-                        var = var + 1
-                except Exception:
-                    pass
-                try:
-                    if grid[fila][colum+1] == 0:
-                        var = var + 1
-                except Exception:
-                    pass
-                try:
-                    if grid[fila+1][colum] == 0:
-                        var = var + 1
-                except Exception:
-                    pass
+    var = None
+    if len(grid) < 100:
+        for fila in range(len(grid)):
+            for colum in range(len(grid[fila])):
+                if grid[fila][colum] == 1:
+                    try:
+                        if grid[fila-1][colum] == 0:
+                            var = var + 1
+                    except Exception:
+                        pass
+                    try:
+                        if grid[fila][colum-1] == 0:
+                            var = var + 1
+                    except Exception:
+                        pass
+                    try:
+                        if grid[fila][colum+1] == 0:
+                            var = var + 1
+                    except Exception:
+                        pass
+                    try:
+                        if grid[fila+1][colum] == 0:
+                            var = var + 1
+                    except Exception:
+                        pass
     return var
